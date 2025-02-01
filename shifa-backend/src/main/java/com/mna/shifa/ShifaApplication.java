8b4e3b4d8912740035522b493ff8f3bc063d2cdf
Md Nisar Ahmed <iamfullstackdev99@gmail.com>
2025-02-02 00:57:26 +0530

    Init: created and intilialized project

---
+++ shifa-backend/src/main/java/com/mna/shifa/ShifaApplication.java
@@ -0,0 +1,13 @@
+package com.mna.shifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShifaApplication.class, args);
+	}
+
+}
---
+++ shifa-backend/src/test/java/com/mna/shifa/ShifaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mna.shifa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShifaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
