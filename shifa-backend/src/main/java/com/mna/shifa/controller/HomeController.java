6aef8e64060e1642fdb8c5a89c98b36a6119fdd0
Md Nisar Ahmed <iamfullstackdev99@gmail.com>
2025-02-02 06:14:14 +0530

    Bug: Fixed default path issue

--- shifa-backend/src/main/java/com/mna/shifa/controller/HomeController.java
+++ shifa-backend/src/main/java/com/mna/shifa/controller/HomeController.java
@@ -1,12 +1,15 @@
 package com.mna.shifa.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("")
+@RestController
+@RequestMapping("/")
 public class HomeController {
 
+    @GetMapping("")
     public String welcome() {
         return "Welcome to Shifa!";
     }
